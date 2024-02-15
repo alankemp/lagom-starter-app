bbf85f2f215e97f600f1ffa6902ef3cfe75bace6
Alan Kemp <alan@alankemp.com>
2024-02-15 22:09:10 +0000

    A simple app using Lagom libraries from Serenity

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## lagom-starter-app
+
+A template project to get you started quickly building an app using the Serenity Lagom libraries.
+
+# Configure
+$ cmake .
+
+# Build
+$ cmake --build .
---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include <LibMain/Main.h>
+#include <LibWebSocket/WebSocket.h>
+
+
+// BUG!  TIFFGenerator.py does not create target directory, leading to FileNotFound errors
+// /home/alan/dev/remote-ladybird/serenity/Userland/Libraries/LibGfx/ImageFormats
+
+ErrorOr<int> serenity_main(Main::Arguments)
+{
+
+
+    return 42;
+}
